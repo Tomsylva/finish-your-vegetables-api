03de57c686490a60b0d690572ee1ceb63e84a03d
Tomsylva <tomsylvanz@gmail.com>
2021-06-07 08:47:33 +0200

    protected meal.push on server side

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const Meal = require("../models/Meal.model");
 const Restaurant = require("../models/Restaurant.model");
 const isLoggedIn = require("../middleware/isLoggedIn");
+const { response } = require("express");
 
 router.get("/", (req, res) => {
   Restaurant.find({}).then((allRestaurants) => {
@@ -18,23 +19,61 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/meal", isLoggedIn, (req, res) => {
-  const { mealName, description, otherInfo, mealType, price, restaurant } =
-    req.body;
-  Meal.create({
+  const {
     mealName,
     description,
     otherInfo,
     mealType,
     price,
-  })
-    .then((response) => {
-      console.log("response from restaurant.js: ", response);
-      Restaurant.findByIdAndUpdate(restaurant, {
-        $push: { meals: response._id },
-      }).catch((err) => {
-        console.error(err);
-      });
-      res.json(response);
+    restaurant,
+    userId,
+  } = req.body;
+
+  // console.log("userId", userId);
+  // console.log("restaurant:", restaurant);
+
+  // Meal.create({
+  //   mealName,
+  //   description,
+  //   otherInfo,
+  //   mealType,
+  //   price,
+  // })
+  //   .then((response) => {
+  //     console.log("response from restaurant.js: ", response);
+  //     Restaurant.findByIdAndUpdate(restaurant, {
+  //       $push: { meals: response._id },
+  //     }).catch((err) => {
+  //       console.error(err);
+  //     });
+  //     res.json(response);
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //   });
+
+  Restaurant.findById(restaurant)
+    .then((foundRestaurant) => {
+      if (foundRestaurant.owner == userId) {
+        Meal.create({
+          mealName,
+          description,
+          otherInfo,
+          mealType,
+          price,
+        })
+          .then((response) => {
+            Restaurant.findByIdAndUpdate(restaurant, {
+              $push: { meals: response._id },
+            }).catch((err) => {
+              console.error(err);
+            });
+            res.json(response);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      }
     })
     .catch((err) => {
       console.error(err);
